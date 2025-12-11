a79de610fae186807b77ff3384228777a56decc8
Abdallah Eid <abdallaheeid@icloud.com>
2025-12-11 22:03:18 +0100

    attemp 3

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt update -y
 sudo apt install -y zlib1g-dev uuid-dev libmnl-dev gcc make autoconf automake pkg-config curl git python3-jsonschema jq libuv1-dev libyaml-dev libjson-c-dev
 
 # Install Netdata
-sudo bash <(curl https://get.netdata.cloud/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token xMQWyCrKe-bW-d9DVhqRqplQkbqkGrQXiVR_dPnJCsMx4L4dTBfImOwJzs85GGBoKGeSxQ6oxJ3wLds9St6S8jOb0xIZgNyWSW2hvlASjVaZgL_JdzKVyXpzLjSANZCUvlHM2-o --claim-rooms a3e91404-efb7-4e47-99f4-b831baa7354c --claim-url https://app.netdata.cloud)
+sudo curl https://get.netdata.cloud/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --claim-token xMQWyCrKe-bW-d9DVhqRqplQkbqkGrQXiVR_dPnJCsMx4L4dTBfImOwJzs85GGBoKGeSxQ6oxJ3wLds9St6S8jOb0xIZgNyWSW2hvlASjVaZgL_JdzKVyXpzLjSANZCUvlHM2-o --claim-rooms a3e91404-efb7-4e47-99f4-b831baa7354c --claim-url https://app.netdata.cloud
 
 # Start Netdata
 sudo systemctl start netdata
